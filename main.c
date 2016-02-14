7d807dc134e028885861fa45b08f379889309277
William Oberndorfer <will.oberndorfer@gmail.com>
2016-02-13 20:07:45 -0500

    clean up

--- main.c
+++ main.c
@@ -14,19 +14,22 @@
 
 volatile enum led_status { LED_OFF, LED_ON } led_flag;
 
+/* interrupts when a pin change is detected on PB0 */
 ISR(PCINT0_vect)
 {
 	if (bit_is_clear(PINB, HALL_PIN)) {
-		led_flag = LED_OFF;
+		led_flag = LED_ON;
 	}
 	else {
-		led_flag = LED_ON;
+		led_flag = LED_OFF;
 	}
 }
 
 int main(void)
 {
+	/* set initial state to off */
 	led_flag = LED_OFF;
+	
 	/* set led pin for output, hall pin for input */
 	DDRB |= (1 << LED_PIN);
 	DDRB &= ~(1 << HALL_PIN);
@@ -42,14 +45,16 @@ int main(void)
 	set_sleep_mode(SLEEP_MODE_IDLE);
 	
 	while (1) {
+		/* go to sleep until interrupt */
 		sleep_mode();
-		
+
+		/* turn led on or off based on flag set by interrupt */
 		switch (led_flag) {
 		case LED_OFF:
-			PORTB |= (1 << LED_PIN);
+			PORTB &= ~(1 << LED_PIN);
 			break;
 		case LED_ON:
-			PORTB &= ~(1 << LED_PIN);
+			PORTB |= (1 << LED_PIN);
 			break;
 		default:
 			PORTB &= ~(1 << LED_PIN);
